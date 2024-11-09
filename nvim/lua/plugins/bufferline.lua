aeab5556206662167892e54367e83e808e0f311a
= <ronizebua.rz@gmail.com>
2024-11-09 10:08:01 +0700

    `adjust:` plugins
    - [x] Remove `event = "VeryLazy"` in `bufferline.lua`
    - [x] Add `themable = true` in `bufferline.lua`
    - [x] Remove `add_blank_line_at_top = true` in `neo-tree.lua`
    - [x] Add `disable_move = true` in `startup.lua`

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,13 @@
 return {
   'akinsho/bufferline.nvim',
   version = "*",
-  event = "VeryLazy",
+  -- event = "VeryLazy",
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
   opts = {
     options = {
+      themable = true,
       offsets = {
         {
           filetype = "neo-tree",
